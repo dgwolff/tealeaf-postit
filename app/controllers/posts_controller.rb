9d361d7e0f82b1b262643961d3d41e081720cbd0
Dylan Wolff <dylanwolff@gmail.com>
2015-06-06 19:22:44 +1000

    Add Edit Post functionality, move form to partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,12 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
