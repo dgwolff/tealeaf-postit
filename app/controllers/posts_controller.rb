085448fb951c15e98e2ba3a891146d914a81e6f9
Dylan Wolff <dylanwolff@gmail.com>
2015-06-06 19:24:21 +1000

    Reorder private methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,11 +38,11 @@ class PostsController < ApplicationController
 
   private
 
-  def set_post
-    @post = Post.find(params[:id])
-  end
-
   def post_params
     params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
