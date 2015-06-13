6bb7ff47f34846e9e7be644b45b5bdb7557b2328
Dylan Wolff <dylanwolff@gmail.com>
2015-06-13 16:06:19 +1000

    Add new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :comments
 
   has_secure_password validations: false
+
+  validates :username, presence: true, length: { maximum: 20 },
+            uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
