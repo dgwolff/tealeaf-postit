58020f9f06dd413777c3ee9037d1eb0cbab1b8b4
Dylan Wolff <dylanwolff@gmail.com>
2015-06-13 13:40:21 +1000

    Add has_secure_password to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531000450) do
+ActiveRecord::Schema.define(version: 20150613032154) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -45,8 +45,9 @@ ActiveRecord::Schema.define(version: 20150531000450) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
