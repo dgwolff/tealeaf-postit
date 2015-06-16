c06dbc9297f637254da952d0f1b9b490d36badee
Dylan Wolff <dylanwolff@gmail.com>
2015-06-16 18:17:58 +1000

    Modify error messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,6 @@ class CommentsController < ApplicationController
       flash[:notice] = "Your comment was added"
       redirect_to post_path(@post)
     else
-      flash[:danger] = "You can't submit a blank comment"
       redirect_to :back
     end
   end
