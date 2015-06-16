0ea56c4020cddaf16f6e71aa3ab0eb0060e52562
Dylan Wolff <dylanwolff@gmail.com>
2015-06-16 16:47:00 +1000

    Setup seeds to generate Users, Categories and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Users
+2.times do
+  name = Faker::Name.first_name
+  password = "password"
+  User.create!(username: name,
+               password: password,
+               password_confirmation: password)
+end
+
+# Categories
+2.times do
+  name = Faker::Hacker.ingverb
+  Category.create!(name: name)
+end
+
+
+# Posts
+users = User.order(:created_at).take(1)
+2.times do
+  url = Faker::Internet.url
+  title = Faker::Company.catch_phrase
+  description = Faker::Company.bs
+  created_at = Faker::Date.backward(14)
+  category_ids = Category.pluck(:id).sample(1)
+  users.each { |user| user.posts.create!(url: url,
+                                         title: title,
+                                         description: description,
+                                         created_at: created_at,
+                                         category_ids: category_ids) }
+end
+
+# Comments
+
+# Post Votes
+
+# Comment Votes
