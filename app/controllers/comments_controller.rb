a5d3e5f2b2a49e43f9038220f8598ec93ff9f653
Dylan Wolff <dylanwolff@gmail.com>
2015-07-01 16:34:52 +1000

    Fix for comment validation errors not showing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
       flash[:notice] = "Your comment was added"
       redirect_to post_path(@post)
     else
-      redirect_to :back
+      render 'posts/show'
     end
   end
 
