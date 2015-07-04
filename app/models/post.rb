9c48f9608e3e86251c9fddf113b5cf7faba4845f
Dylan Wolff <dylanwolff@gmail.com>
2015-07-04 18:56:39 +1000

    Implement better slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,13 +10,36 @@ class Post < ActiveRecord::Base
   validates :url, presence: true
   validates :description, presence: true, uniqueness: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   def to_param
     slug
   end
 
-  def generate_slug
-    self.slug = title.gsub(" ", "-").downcase
+  def generate_slug!
+    the_slug = to_slug(title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split("-").last.to_i != 0
+      return str.split("-").slice(0...-1).join("-") + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub!(/\s*[^A-Za-z0-9]\s*/, "-")
+    str.gsub!(/-+/, "-")
+    str.downcase
   end
 end
