1a6596c2da3015ebac443ea71b1144c72410e8f3
Dylan Wolff <dylanwolff@gmail.com>
2015-07-05 12:14:36 +1000

    Only post creator or admin can edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ require 'will_paginate/array'
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:show, :index]
+  before_action :require_creator, only: [:edit, :update]
 
   def index
     @posts = Post.all.sort_by { |x| x.total_votes }.reverse
@@ -65,5 +66,10 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find_by(slug: params[:id])
+  end
+
+  def require_creator
+    access_denied unless logged_in? && (current_user == @post.creator ||
+                                        current_user.admin?)
   end
 end
